feb2b54b635b85f2f5b6dc90feb4c62b5c99a1ea
Will Szumski <will@cowboycoders.org>
2012-09-24 18:04:10 +0100

    removed touch specific stuff

--- EnhancedDigitalOutputApplet.lua
+++ EnhancedDigitalOutputApplet.lua
@@ -81,13 +81,13 @@ function deviceMenu(self, menuItem, firstUse)
 									   local window = Window("text_list", menuItem.text)
 									   local menu = SimpleMenu("menu")
 									   menu:setHeaderWidget(Textarea("help_text", self:string("HELP_USB1_DAC")))
-									   menu:addItem({
-											text = self:string("USB1_DAC_NO_HUB"),
-											style = 'item_choice',
-											callback = function(event, menuItem)
-														   self:_setCardAndReboot(card.id, true)
-													   end,
-									   })
+									  -- menu:addItem({
+										--	text = self:string("USB1_DAC_NO_HUB"),
+											--style = 'item_choice',
+											--callback = function(event, menuItem)
+												--		   self:_setCardAndReboot(card.id, true)
+													--   end,
+									   --})
 									   menu:addItem({
 											text = self:string("USB1_DAC_HUB"),
 											style = 'item_choice',
@@ -102,10 +102,10 @@ function deviceMenu(self, menuItem, firstUse)
 								   end
 							   end,
 				}
-			elseif card.id == "TXRX" then
-				items[#items+1] = {
-					text = card.desc .. tostring(self:string("CURRENT")),
-				}
+			--elseif card.id == "TXRX" then
+			--	items[#items+1] = {
+			--		text = card.desc .. tostring(self:string("CURRENT")),
+			--	}
 			else
 				items[#items+1] = {
 					text = card.desc .. tostring(self:string("INFO")),
@@ -164,83 +164,8 @@ function optionsMenu(self, menuItem)
 				   end
 	})
 
-	menu:addItem({
-		text = self:string("KERNEL_UPDATE"),
-		sound = "WINDOWSHOW",
-		callback = function(event, menuItem)
-					   local window = Window("text_list", menuItem.text)
-					   local menu = SimpleMenu("menu")
-					   menu:setHeaderWidget(Textarea("help_text", self:string("HELP_KERNEL_UPDATE")))
-					   menu:addItem({
-							text = self:string("KERNEL_UPDATE_AUTO"),
-							style = 'item_choice',
-							check = Checkbox("checkbox",
-								  function(object, isSelected)
-									  self:getSettings()["autoKernelUpdate"] = isSelected
-									  self:storeSettings()
-								  end,
-								  self:getSettings()["autoKernelUpdate"]
-							),
-					   })
-					   menu:addItem({
-							text = self:string("KERNEL_UPDATE_NOW"),
-							style = 'item_choice',
-							callback = function(event, menuItem)
-										   self:_kernelUpdate()
-									   end,
-					   })
-					   window:addWidget(menu)
-					   window:show()
-				   end,
-	})
 
-	menu:addItem({
-		text = self:string("NO_HUB_OPTION"),
-		sound = "WINDOWSHOW",
-		callback = function(event, menuItem)
-					   local window = Window("text_list", menuItem.text)
-					   local menu = SimpleMenu("menu")
-					   menu:setHeaderWidget(Textarea("help_text", self:string("HELP_NO_HUB_OPTION"))) 
-					   menu:addItem({
-							text = self:string("NO_HUB_SETTING"),
-							style = 'item_choice',
-							check = Checkbox("checkbox",
-								  function(object, isSelected)
-									  self:getSettings()["embeddedTTHack"] = isSelected
-									  self:storeSettings()
-									  self:_restart()
-								  end,
-								  self:getSettings()["embeddedTTHack"]
-							),
-					   })
-					   window:addWidget(menu)
-					   window:show()
-				   end,
-	})
 
-	menu:addItem({
-		text = self:string("KERNEL_IDLE_OPTION"),
-		sound = "WINDOWSHOW",
-		callback = function(event, menuItem)
-					   local window = Window("text_list", menuItem.text)
-					   local menu = SimpleMenu("menu")
-					   menu:setHeaderWidget(Textarea("help_text", self:string("HELP_KERNEL_IDLE_OPTION")))
-					   menu:addItem({
-							text = self:string("KERNEL_IDLE_SETTING"),
-							style = 'item_choice',
-							check = Checkbox("checkbox",
-								  function(object, isSelected)
-									  self:getSettings()["cpuIdleFullspeed"] = isSelected
-									  self:storeSettings()
-									  self.cpuIdle() -- cb in meta to potentially update state
-								  end,
-								  self:getSettings()["cpuIdleFullspeed"]
-							),
-					   })
-					   window:addWidget(menu)
-					   window:show()
-				   end,
-	})
 
 	menu:addItem({
 		text = self:string("BUFFER_TUNING"),
@@ -292,9 +217,6 @@ function _parseCards(self)
 		log:error("/proc/asound/cards could not be opened")
 		return
 	end
-
-	-- internal cards, put first in list
-	t[1] = { id = "TXRX", desc = "Digital Only" }
 
 	-- read and parse entries
 	for line in cards:lines() do
@@ -393,7 +315,8 @@ function _parseStreamInfo(self, card)
 
 		-- touch needs a hub in this case only due to imx35 embedded TT limitations
 		if type == "ASYNC" and t.speed == "full" and not t.hub then
-			t.needshub = true
+			--t.needshub = true
+            t.needshub = false
 		end
 
 	end
@@ -410,7 +333,6 @@ function _setCardAndReboot(self, card, embeddedTTHack)
 	local s = self:getSettings()
 
 	s.playbackDevice = card
-	s.embeddedTTHack = embeddedTTHack
 
 	self:storeSettings()
 
@@ -506,145 +428,10 @@ function _restart(self)
 	self.timer = Timer(3000,
 					   function()
 						   log:info("rebooting...")
-						   appletManager:callService("reboot")
+						   os.execute("sudo shutdown -r now")
 					   end,
 					   true)
 	self.timer:start()
 end
 
 
---------------------------------------------------------------------------------------------------------------------------------
--- following is a modified form of SetupFirmwareUpgrade
-
-function _kernelUpdate(self, kernel)
-	if kernel then
-		self.kernel = kernel
-	end
-
-	self.popup = Popup("update_popup")
-	self.icon = Icon("icon_software_update")
-	self.popup:addWidget(self.icon)
-
-	self.text = Label("text", self:string("DOWNLOADING"), "")
-	self.counter = Label("subtext", "")
-	self.progress = Slider("progress", 1, 100, 1)
-
-	self.popup:addWidget(self.text)
-	self.popup:addWidget(self.counter)
-	self.popup:addWidget(self.progress)
-	self.popup:focusWidget(self.text)
-
-	-- make sure this popup remains on screen
-	self.popup:setAllowScreensaver(false)
-	self.popup:setAlwaysOnTop(true)
-	self.popup:setAutoHide(false)
-	self.popup:setTransparent(false)
-
-	-- no way to exit this popup
-	self.upgradeListener =
-		Framework:addListener(EVENT_ALL_INPUT,
-				      function()
-					      Framework.wakeup()
-					      return EVENT_CONSUME
-				      end,
-				      true)
-
-	-- disconnect from SqueezeCenter, we don't want to up
-	-- interrupted during the firmware upgrade.
-	appletManager:callService("disconnectPlayer")
-
-	-- stop memory hungry services before upgrading
-	if (System:getMachine() == "fab4") then
-
-		appletManager:callService("stopSqueezeCenter")
-		appletManager:callService("stopFileSharing")
-
-		-- start the upgrade once SBS is shut down or timed out
-		local timeout = 0
-		self.serverStopTimer = self.popup:addTimer(1000, function()
-
-			timeout = timeout + 1
-			
-			if timeout <= STOP_SERVER_TIMEOUT and appletManager:callService("isBuiltInSCRunning") then
-				return
-			end
-
-			Task("upgrade", self, _doUpgrade, _upgradeFailed):addTask()
-			
-			self.popup:removeTimer(self.serverStopTimer)
-		end)
-	else
-		Task("upgrade", self, _doUpgrade, _upgradeFailed):addTask()
-	end
-
-	self:tieAndShowWindow(self.popup)
-	return window
-end
-
-
-function _doUpgrade(self)
-	Task:yield(true)
-
-	-- EN only messages
-	local str = { UPDATE_DOWNLOAD = self:string("KERNEL_DOWNLOAD"), UPDATE_VERIFY = self:string("KERNEL_VERIFY"), 
-				  UPDATE_REBOOT = self:string("REBOOTING") }
-
-	local KernelUpgrade = require("applets.EnhancedDigitalOutput.KernelUpgrade")
-
-	local t, err = KernelUpgrade():start(self.kernel.url, self.kernel.md5, 
-		function (done, msg, count)
-			if type(count) == "number" then
-				if count >= 100 then
-					count = 100
-				end
-				self.counter:setValue(count .. "%")
-				self.progress:setRange(1, 100, count)
-			else
-				self.counter:setValue("")
-			end
-			
-			self.text:setValue(str[msg] or msg)
-			
-			if done then
-				self.icon:setStyle("icon_restart")
-			end
-		end
-	)
-	if not t then
-		log:error("Upgrade failed: ", err)
-		self:_upgradeFailed()
-
-		if self.popup then
-			self.popup:hide()
-			self.popup = nil
-		end
-	end
-end
-
-
-function _upgradeFailed(self)
-	-- unblock keys
-	Framework:removeListener(self.upgradeListener)
-	self.upgradeListener = nil
-
-	-- reconnect to server
-	appletManager:callService("connectPlayer")
-
-	local window = Window("help_list", self:string("KERNEL_UPDATE_FAILED"))
-	local menu = SimpleMenu("menu")
-	window:addWidget(menu)
-
-	menu:addItem({
-		text = self:string("CANCEL"),
-		sound = "WINDOWHIDE",
-		callback = function()
-					   window:hide()
-				   end,
-	})
-
-	-- turn off auto update
-	self:getSettings()["autoKernelUpdate"] = false
-	self:storeSettings()
-
-	self:tieAndShowWindow(window)
-end
--- EnhancedDigitalOutputMeta.lua
+++ EnhancedDigitalOutputMeta.lua
@@ -25,13 +25,9 @@ local jiveMain, jnt, string, tonumber = jiveMain, jnt, string, tonumber
 module(...)
 oo.class(_M, AppletMeta)
 
--- custom kernel configuration
-local USB_KERNEL_VERSION = 1
-local USB_KERNEL_URL     = "http://triodeplugins.googlecode.com/files/kernel-digitalout-1.bin"
-local USB_KERNEL_MD5     = "b1df8c851322d1b2bba0d1507e7e8f2e"
 
 
-local playerPower, cpuActive
+local playerPower
 
 
 function jiveVersion(meta)
@@ -44,9 +40,6 @@ function defaultSettings(self)
 		playbackDevice = "default",
 		bufferTime = 20000,
 		periodCount = 2,
-		autoKernelUpdate = true,
-		embeddedTTHack = false,
-		cpuIdleFullspeed = false,
 		firstUse = true
 	}
 end
@@ -54,54 +47,18 @@ end
 
 function registerApplet(meta)
 	local settings = meta:getSettings()
-	local updating
-	
-	-- check kernel version and install if required, with safety checks...
-	local fh1 = io.open("/proc/version")
-	if fh1 then
-		local text = fh1:read("*a")
-		local ver  = string.match(text, "#(%d+)%[usb%]")
+    local updating = false
 
-		if ver == nil or tonumber(ver) < USB_KERNEL_VERSION then
-			log:info("kernel is not latest usb kernel")
-			-- check we are enabled by appletInstaller to allow kernel update to be bypassed on factory reset
-			local enabled = false
-			local fh2 = io.open("/etc/squeezeplay/userpath/settings/SetupAppletInstaller.lua")
-			if fh2 then
-				local text = fh2:read("*a")
-				if string.match(text, "EnhancedDigitalOutput") then
-					enabled = true
-				end
-				fh2:close()
-			end
-			if enabled and settings.autoKernelUpdate then
-				log:info("attempting to update kernel")
-				local applet = appletManager:loadApplet('EnhancedDigitalOutput')
-				applet:_kernelUpdate({ url = USB_KERNEL_URL, md5 = USB_KERNEL_MD5 })
-				updating = true
-			else
-				log:warn("kernel does not support usb extensions")
-			end
-
-		else
-			log:info("lastest usb kernel found")
-			fh1:close()
-		end
-	end
 
 	-- check if this is a new install and if so ensure custom jive_alsa is installed
-	local newbinary = io.open("/usr/share/jive/applets/EnhancedDigitalOutput/jive_alsa", "r")
+	local newbinary = io.open("/opt/squeezeplay/share/jive/applets/EnhancedDigitalOutput/jive_alsa", "r")
 	if not updating and newbinary then
 		log:info("installing modified jive_alsa")
 		newbinary:close()
-		os.execute("mv /usr/share/jive/applets/EnhancedDigitalOutput/jive_alsa /usr/bin/jive_alsa")
-		os.execute("chmod 755 /usr/bin/jive_alsa")
+		os.execute("mv /opt/squeezeplay/share/jive/applets/EnhancedDigitalOutput/jive_alsa /usr/bin/jive_alsa")
+		os.execute("chmod 755 /opt/squeezeplay/bin/jive_alsa")
 	end
 
-	-- if usb hack then set kernel option
-	if settings.embeddedTTHack then
-		_write("/sys/module/snd_usb_audio/parameters/async_embedded_tt_hack", "1")
-	end
 
 	-- check output device is available else bring up popup and restart if output attaches
 	local playbackDeviceFound = true
@@ -135,7 +92,7 @@ function registerApplet(meta)
 						rebootTime = rebootTime - 1
 						if rebootTime == 0 then
 							log:info("rebooting")
-							appletManager:callService("reboot")
+							os.execute("sudo reboot")
 						end
 					end
 					
@@ -164,17 +121,17 @@ function registerApplet(meta)
 		})
 		
 		-- if spdif or usb output increase priority of relavent irq task
-		if settings.playbackDevice == "TXRX" then
-			os.execute("chrt -f -p 59 `pidof 'IRQ-47'`")
-		elseif settings.playbackDevice != "default" then
-			os.execute("chrt -f -p 59 `pidof 'IRQ-37'`")
-		end
+		--if settings.playbackDevice == "TXRX" then
+		--	os.execute("chrt -f -p 59 `pidof 'IRQ-47'`")
+		--elseif settings.playbackDevice != "default" then
+		--	os.execute("chrt -f -p 59 `pidof 'IRQ-37'`")
+		--end
 	
 	end
 
 	-- register with NetworkThread to get callbacks on for player on/off and audio active/inactive
-	jnt:subscribe(meta)
-	jnt:registerCpuActive(function(active) cpuActive = active meta:cpuIdle() end)
+	--jnt:subscribe(meta)
+	--jnt:registerCpuActive(function(active) cpuActive = active meta:cpuIdle() end)
 
 	-- register menus
 	jiveMain:addItem(
@@ -185,8 +142,6 @@ function registerApplet(meta)
 	jiveMain:addItem(
 		meta:menuItem('appletEnhancedDigitalOutputOptions', 'advancedSettings', meta:string("APPLET_NAME"), 
 			function(applet, ...) 
-				applet.cpuIdle = function() meta:cpuIdle(true) end
-				applet.kernel = { url = USB_KERNEL_URL, md5 = USB_KERNEL_MD5 }
 				applet:optionsMenu(...) 
 			end
 		)
@@ -200,24 +155,6 @@ function registerApplet(meta)
 		applet:deviceMenu(nil, true)
 	end
 
-end
-
-
-function notify_playerPower(self, player, power)
-	if player:isLocal() then
-		playerPower = power
-		self:cpuIdle()
-	end
-end
-
-
-function cpuIdle(self, force)
-	local filename = "/sys/power/pm_idle_fullspeed"
-	local enabled  = self:getSettings()["cpuIdleFullspeed"]
-	local setting  = (enabled and playerPower and cpuActive) and "1" or "0"
-	if enabled or force then
-		_write(filename, setting)
-	end
 end
 
 
